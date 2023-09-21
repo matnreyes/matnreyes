4141e2fc4d74b075caad51e2327480bc4b9bb9c0
matnreyes <matias_reyes@icloud.com>
2023-09-20 23:09:16 -0400

    add header

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,7 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Matt Reyes - Official Site" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
@@ -10,9 +10,14 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div>
-      <div className="bg-blue-950 text-white h-[20vh] flex justify-center items-center">
-        <h1 className="font-light text-4xl font-sans">Matt Reyes</h1>
-        <h2>Efficient, detailed, and diverse</h2>
+      <div className="bg-blue-950 text-white h-[20vh] flex items-center px-8">
+        <div className="max-w-[50vw]">
+          <h1 className="font-light text-4xl font-sans">Hey, I'm Matt!</h1>
+          <h2>I'm a full stack developer passionate about finding efficient and creative solutions to everyday problems</h2>
+        </div>
+        <div>
+          image here
+        </div>
       </div>
     </div>
   );
